269b203c2dffb248492d0d973040c1ea60b99a4d
aUp99 <popandopulonastya99@mail.ru>
2022-05-19 20:50:59 +0300

    add all files

---
+++ test_main_page.py
@@ -0,0 +1,14 @@
+from FinalProject.pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+def test_quest_should_be_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
